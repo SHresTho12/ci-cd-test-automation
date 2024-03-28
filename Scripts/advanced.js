723c478995ab9a8012745991a89240e87fd32f04
SHresTho12 <talimul12bari@gmail.com>
2024-03-28 15:06:39 +0600

    Calculator fixed

--- Scripts/advanced.js
+++ Scripts/advanced.js
@@ -1,7 +1,14 @@
-function pow (a,b){
-    return a ^ b;
+function pow(a, b) {
+  console.log("pow called with " + a + " and " + b);
+  result = a ** b;
+  console.log("result is " + result);
+  return a ** b;
 }
 
-function modulo (a,b){
-    return a % b;
+function modulo(a, b) {
+  return a % b;
 }
+module.exports = {
+  pow,
+  modulo,
+};
